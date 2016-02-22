83b755c22716df970792d53acd8591effed2d21a
rvrv <lukasgrand@gmail.com>
2016-02-22 15:09:33 +0100

    Update FilterLink.jsx

--- client/components/FilterLink.jsx
+++ client/components/FilterLink.jsx
@@ -1,7 +1,7 @@
 FilterLink = ({ dispatch, filter, visibilityFilter, children }) => {
   const setFilter = () => {
     dispatch(setVisibilityFilter(filter));
-    dispatch(dispatch({type: 'CHANGE_PAGE', currentPageNumber: 0}));
+    dispatch({type: 'CHANGE_PAGE', currentPageNumber: 0});
   }
   return (
     <Link active={visibilityFilter === filter} onClick={()=> {return setFilter()}}>
