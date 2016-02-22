a6a3b1962ff9cd9633a02fb4566ecd9dde980c9e
rvrv <lukasgrand@gmail.com>
2016-02-22 15:16:12 +0100

    make use of changePage action

--- client/components/FilterLink.jsx
+++ client/components/FilterLink.jsx
@@ -1,7 +1,7 @@
 FilterLink = ({ dispatch, filter, visibilityFilter, children }) => {
   const setFilter = () => {
     dispatch(setVisibilityFilter(filter));
-    dispatch({type: 'CHANGE_PAGE', currentPageNumber: 0});
+    dispatch(changePage(0));
   }
   return (
     <Link active={visibilityFilter === filter} onClick={()=> {return setFilter()}}>
