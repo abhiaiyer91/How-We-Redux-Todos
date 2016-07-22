19bb1d108cf625e56b6c71c1dfedcc64b71c3928
Simon Tucker <srtucker22@gmail.com>
2016-07-22 12:08:19 -0400

    devtools (#11)
    * devtools
    
    * latest meteor updates

--- imports/client/store/store.js
+++ imports/client/store/store.js
@@ -1,9 +1,16 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import createLogger from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from '../../../imports/client/reducers/rootReducer';
+import DevTools from '../../../imports/client/components/DevTools';
+
 const logger = createLogger();
 
-const Store = createStore(rootReducer, {}, applyMiddleware(ReduxThunk, logger));
+const enhancers = [
+  applyMiddleware(ReduxThunk, logger),
+  DevTools.instrument()
+];
+
+const Store = createStore(rootReducer, {}, compose(...enhancers));
 
 export default Store;
